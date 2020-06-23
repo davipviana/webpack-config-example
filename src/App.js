0b3dbeceea7e26dfc1a85a02bb8e924dd646a875
Davi Viana <davipviana@gmail.com>
2020-06-23 20:58:22 -0300

    basic react app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component, Suspense } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Users from './containers/Users';
+
+const AsyncPizza = React.lazy(() => {
+  return import('./containers/Pizza.js');
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Users</Link>
+          <Link to="/pizza">Pizza</Link>
+        </div>
+        <div>
+          <Route path="/" exact component={Users} />
+          <Route path="/pizza" render={() => (
+            <Suspense fallback={<div>Loading...</div>}>
+              {AsyncPizza}
+            </Suspense>
+          )} />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Pizza from '../../assets/pizza.jpg';
+
+import styles from './PizzaImage.module.css';
+
+const PizzaImage = (props) => (
+  <div className={styles.PizzaImage}>
+    <img src={Pizza} className={styles.PizzaImg} />
+  </div>
+);
+
+export default PizzaImage;
